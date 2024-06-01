eec17e8e41c345014a96fc885c9af0d1dd3d5fe8
Sofiya Dush <svetgk1@mail.ru>
2024-06-01 19:27:55 +0300

    Add commit

--- test_var6.py
+++ test_var6.py
@@ -16,11 +16,12 @@ def test_get_var6_fare_single_line():
 
 
 def test_get_var6_fare_multiple_lines():
-    lines = ["1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S",
-             "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,71.2833,C85,C",
-             "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S",
-             "4,1,1,'Futrelle, Mrs. Jacques Heath (Lily May Peel)',female,35,1,0,113803,53.1,C123,S"]
-    actual = get_var6(lines, 'S')
-    expected = (30.5125, 7.25)
+    lines = ["1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,5,,S",
+        "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,100,C85,C",
+        "32,0,1,'Spencer, Mrs. William Augustus (Marie Eugenie)',female,,1,0,PC 17569,50,B78,C",
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,10,,S",
+        "4,1,1,'Futrelle, Mrs. Jacques Heath (Lily May Peel)',female,35,1,0,113803,15,C123,S"]
+    actual = get_var6(lines, "S")
+    expected = (12.5, 5.0)
 
     assert actual == expected
